61f91cf00cf73174fd05631ed61e70f583e02789
Hasan <hasan.akniyev@gmail.com>
2015-12-11 01:28:28 +0300

    Added brands to catalog and created draft brand/ and brands/brandname pages

---
+++ bitrix/templates/arteva/components/bitrix/catalog.section/section_items_brand/template.php
@@ -0,0 +1,213 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+
+//	var_dump($arResult);
+	// Получаем все родительские разделы текущего раздела
+	$SecParants1 = GetAllParantsSection($arResult['ID']); 
+
+	$APRT_DATA = "";
+	if( count($SecParants1) > 1)
+	{
+		$APRT_DATA = "parentCategories: [";
+		foreach($SecParants1 as $k => $SecPar)
+			if($k != 0)	$APRT_DATA .= "{id: " . $SecPar["ID"] . ", name: '" . $SecPar["NAME"] . "'},";
+		
+		$APRT_DATA .= "]";
+	}
+?>
+<script>
+	// Интеграция с retailrocket.ru
+    rrApiOnReady.push(function() {
+		try { rrApi.categoryView(<?= $arResult['ID']?>); } catch(e) {}
+	})
+	
+	
+	window.ad_category = "<?= $arResult['ID']?>";
+
+	window.APRT_DATA = {
+		pageType: 3,
+		currentCategory: {
+			id: <?= $arResult['ID']?>,
+			name: "<?=$arResult["NAME"]?>"
+		},
+		<?=$APRT_DATA ?>
+	}
+</script>
+
+
+<?if ($arResult["SEO_PRODUCTS"] && in_array($_REQUEST["page"], $arResult["SEO_PAGE_CODE"])):?>
+	<div class="outer-content-wrapper">
+    	<div class="content-wrapper">
+			<?$APPLICATION->IncludeComponent("intsys:breadcrumb","bread",Array(
+							"START_FROM" => "0",
+							"PATH" => $APPLICATION->GetCurPage(),
+							"SITE_ID" => "s1"
+					)
+			);?>
+		    <h1><?=$arResult["CUR_SEO_PAGE"]["NAME"]?></h1>
+		    <div class="text-content">
+		        <?if ($arResult["CUR_SEO_PAGE"]["~PREVIEW_PICTURE"]):?>
+		            <img src="<?=CFile::GetPath($arResult["CUR_SEO_PAGE"]["~PREVIEW_PICTURE"])?>" alt=""/>
+		        <?endif?>
+		        <?=$arResult["CUR_SEO_PAGE"]["~PREVIEW_TEXT"]?>
+		    </div>
+		    <div class="item-cards-list-cnt">
+		        <ul class="item-cards-list matrix">
+		            <?foreach ($arResult["SEO_PRODUCTS"] as $key => $arItems) :?>
+		                <li class="item-card-item">
+		                    <a href="<?=$arItems["DETAIL_PAGE_URL"]?>">
+		                        <?
+		                            //$waterImage = waterImage($arItems["~PREVIEW_PICTURE"]);
+		                            // $waterImage["src"]
+		                            $waterImage["src"] = CFIle::GetPath($arItems["~PREVIEW_PICTURE"]);
+		                        ?>
+		                        <div class="img-cnt">
+		                            <img src="/img/img_dummy.png" data-src="<?=$waterImage["src"]?>" alt=""/>
+		                        </div>
+		                        <div class="item-info">
+		                            <p class="item-brand"><?=$arItems["NAME"]?></p>
+		                            <p class="item-desc">Артикул <?=$arItems["PROPERTY_ARTIKUL_VALUE"]?></p>
+									
+									<?if(isset($_COOKIE["bigbuzzy"]) && $_COOKIE["bigbuzzy"] == "Y" && $arItems["PROPERTIES"]["SALE"]["VALUE"] != 1) {?>
+											<p class="item-price"><span><?=number_format($arItems["PRICES"]["DISCOUNT_VALUE"] * 0.9, 0, 0, " ")?></span> руб.</p>
+											<p class="old-price"><span><?=number_format($arItems["PRICES"]["PRICE"], 0, 0, " ")?></span> руб.</p>
+									<?} else {?>
+										<p class="item-price"><span><?=number_format($arItems["PRICES"]["DISCOUNT_VALUE"], 0, 0, " ")?></span> руб.</p>
+										<?if ($arItems["PRICES"]["DISCOUNT_VALUE"] < $arItems["PRICES"]["PRICE"]):?>
+											<p class="old-price"><span><?=number_format($arItems["PRICES"]["PRICE"], 0, 0, " ")?></span> руб.</p>
+										<?endif?>
+									<?}?>
+									
+		                        </div>
+		                        <?if ($arItems["PROPERTY_HIT_VALUE"]):?>
+		                            <div class="item-card-badge hit">Хит продаж</div>
+		                        <?endif?>
+		                        <?if ($arItems["PROPERTY_NEW_VALUE"]):?>
+		                            <div class="item-card-badge new">Новинка</div>
+		                        <?endif?>
+		                        <?
+		                            $salePersent = 100-($arItems["PRICES"]["DISCOUNT_VALUE"]*100)/$arItems["PRICES"]["PRICE"];
+		                            $salePersent = number_format($salePersent, 0, 0, "");
+		                        ?>
+		                        <?if ($salePersent > 0):?>
+		                            <div class="item-card-badge sale">скидка <?=$salePersent?>%</div>
+		                        <?endif?>
+								<? if(isset($_COOKIE["bigbuzzy"]) && $_COOKIE["bigbuzzy"] == "Y" && $arItems["PROPERTIES"]["SALE"]["VALUE"] != 1){?>
+									<div class="item-card-badge sale">скидка 10% bb</div>
+								<?}?> 
+		                    </a>
+		                </li>
+		            <?endforeach?>
+		        </ul>
+		        <div class="preload-overlay"><i></i></div>
+		    </div>
+		    <?if ($arResult["SEO_PAGES"]):?>
+		        <div class="catalog-tags-cnt">
+		            <ul class="catalog-tag-list">
+		                <?foreach ($arResult["SEO_PAGES"] as $key => $arSeoItems) :?>
+		                    <li class="catalog-tag <?if ($_REQUEST["page"] == $arSeoItems["CODE"]):?>active<?endif?>">
+		                        <a href="<?=$arResult["SECTION_PAGE_URL"]?>?page=<?=$arSeoItems["CODE"]?>"><?=$arSeoItems["NAME"]?></a>
+		                    </li>
+		                <?endforeach?>
+		            </ul>
+		        </div>
+		    <?endif?>
+		</div>
+	</div>
+<?else:?>
+	<div class="outer-content-wrapper">
+    	<div class="content-wrapper">
+			<?$APPLICATION->IncludeComponent("intsys:breadcrumb","bread",Array(
+							"START_FROM" => "0",
+							"PATH" => $APPLICATION->GetCurPage(),
+							"SITE_ID" => "s1"
+					)
+			);?>
+		    <?$APPLICATION->IncludeComponent(
+				"bitrix:catalog.section.list",
+				"section",
+				Array(
+					"IBLOCK_TYPE" => "catalog",
+					"IBLOCK_ID" => "17",
+					"SECTION_ID" => "",
+					"SECTION_CODE" => $arResult["CODE"],
+					"COUNT_ELEMENTS" => "Y",
+					"TOP_DEPTH" => "1",
+					"SECTION_FIELDS" => array("","",""),
+					"SECTION_USER_FIELDS" => array("UF_1C_ID","",""),
+					"VIEW_MODE" => "LINE",
+					"SHOW_PARENT_NAME" => "Y",
+					"SECTION_URL" => "",
+					"CACHE_TYPE" => "A",
+					"CACHE_TIME" => "36000000",
+					"CACHE_GROUPS" => "Y",
+					"ADD_SECTIONS_CHAIN" => "Y"
+				)
+			);?>
+	    </div>
+	</div>
+	<div class="outer-content-wrapper">
+	    <div class="content-wrapper">
+	        <div class="item-cards-list-cnt">
+	            <ul class="item-cards-list matrix">
+	            	<?foreach ($arResult["ITEMS"] as $key => $arItems) :?>
+		                <li class="item-card-item">
+		                    <a href="<?=$arItems["DETAIL_PAGE_URL"]?>">
+		                    	<?
+			                        //$waterImage = waterImage($arItems["~PREVIEW_PICTURE"]);
+			                        // $waterImage["src"]
+			                        $waterImage["src"] = CFIle::GetPath($arItems["~PREVIEW_PICTURE"]);
+			                    ?>
+		                        <div class="img-cnt">
+		                        	<img src="/img/img_dummy.png" data-src="<?=$waterImage["src"]?>" alt=""/>
+		                        </div>
+		                        <div class="item-info">
+		                            <p class="item-brand"><?=$arItems["NAME"]?></p>
+		                            <p class="item-desc">Артикул <?=$arItems["PROPERTIES"]["ARTIKUL"]["VALUE"]?></p>
+									
+									<?if(isset($_COOKIE["bigbuzzy"]) && $_COOKIE["bigbuzzy"] == "Y" && $arItems["PROPERTIES"]["SALE"]["VALUE"] != 1) {?>
+											<p class="item-price"><span><?=number_format($arItems["PRICES"]["BASE"]["DISCOUNT_VALUE"] * 0.9, 0, 0, " ")?></span> руб.</p>
+											<p class="old-price"><span><?=number_format($arItems["PRICES"]["BASE"]["VALUE"], 0, 0, " ")?></span> руб.</p>
+									<?} else {?>
+		                            <p class="item-price"><span><?=number_format($arItems["PRICES"]["BASE"]["DISCOUNT_VALUE"], 0, 0, " ")?></span> руб.</p>
+		                        	<?if ($arItems["PRICES"]["BASE"]["DISCOUNT_VALUE"] < $arItems["PRICES"]["BASE"]["VALUE"]):?>
+			                            <p class="old-price"><span><?=number_format($arItems["PRICES"]["BASE"]["VALUE"], 0, 0, " ")?></span> руб.</p>
+			                        <?endif?>
+									<?}?>
+									
+		                        </div>
+		                        <?if ($arItems["PROPERTIES"]["HIT"]["VALUE"]):?>
+				                	<div class="item-card-badge hit">Хит продаж</div>
+				                <?endif?>
+				                <?if ($arItems["PROPERTIES"]["NEW"]["VALUE"]):?>
+				                	<div class="item-card-badge new">Новинка</div>
+				                <?endif?>
+				                <?
+		                            $salePersent = 100-($arItems["PRICES"]["BASE"]["DISCOUNT_VALUE"]*100)/$arItems["PRICES"]["BASE"]["VALUE"];
+		                            $salePersent = number_format($salePersent, 0, 0, "");
+		                        ?>
+		    	                <?if ($salePersent > 0):?>
+		    	                	<div class="item-card-badge sale">скидка <?=$salePersent?>%</div>
+		    	                <?endif?>
+								<? if(isset($_COOKIE["bigbuzzy"]) && $_COOKIE["bigbuzzy"] == "Y" && $arItems["PROPERTIES"]["SALE"]["VALUE"] != 1){?>
+									<div class="item-card-badge sale">скидка 10% bb</div>
+								<?}?> 
+		                    </a>
+		                </li>
+	               	<?endforeach?>
+	            </ul>
+	        </div>
+	        <?if ($arResult["SEO_PAGES"]):?>
+		        <div class="catalog-tags-cnt">
+		            <ul class="catalog-tag-list">
+		                <?foreach ($arResult["SEO_PAGES"] as $key => $arSeoItems) :?>
+		                    <li class="catalog-tag <?if ($_REQUEST["page"] == $arSeoItems["CODE"]):?>active<?endif?>">
+		                        <a href="<?=$arResult["SECTION_PAGE_URL"]?>?page=<?=$arSeoItems["CODE"]?>"><?=$arSeoItems["NAME"]?></a>
+		                    </li>
+		                <?endforeach?>
+		            </ul>
+		        </div>
+		    <?endif?>
+	    </div>
+	</div>
+<?endif?>
--- bitrix/templates/arteva/components/bitrix/catalog/intsys1/section.php
+++ bitrix/templates/arteva/components/bitrix/catalog/intsys1/section.php
@@ -65,7 +65,7 @@ while (($BRAND = $BRANDS_QUERY->Fetch()) != false) {
 //
 //test_dump($brandFilter);
 
-if (!findSection($current_section, 17) && $section_code != "new" && $section_code != "sale") {
+if (!findSection($current_section, 17) && $section_code != "new" && $section_code != "sale" && $section_code != "brands") {
     include($_SERVER["DOCUMENT_ROOT"] . "/404.php");
     die();
 } elseif (!($third_section_code == false || $brandFilter == true)) {
@@ -87,8 +87,191 @@ if ($brandFilter) {
 //    test_dump($GLOBALS["arrFilterSectionItemsNew"]);
 }
 
+if ($section_code == "brands" && $subsection_code == false) {
+?>
+<div class="outer-content-wrapper">
+    <div class="content-wrapper">
+    	<?$APPLICATION->IncludeComponent(
+	        "bitrix:breadcrumb",
+	        "bread",
+	        Array(
+	            "START_FROM" => "0",
+	            "PATH" => "",
+	            "SITE_ID" => "-"
+	        )
+	    );?>
+        <div class="text-content">
+        	<h1>Наши торговые марки</h1>
+			<?$APPLICATION->IncludeComponent(
+		        "bitrix:news.list",
+		        "brands",
+		        Array(
+		            "IBLOCK_TYPE" => "brands",
+		            "IBLOCK_ID" => "6",
+		            "NEWS_COUNT" => "999",
+		            "SORT_BY1" => "ACTIVE_FROM",
+		            "SORT_ORDER1" => "DESC",
+		            "SORT_BY2" => "SORT",
+		            "SORT_ORDER2" => "ASC",
+		            "FILTER_NAME" => "",
+		            "FIELD_CODE" => array("NAME","PREVIEW_TEXT","PREVIEW_PICTURE","DETAIL_TEXT","DETAIL_PICTURE","DATE_ACTIVE_FROM",""),
+		            "PROPERTY_CODE" => array("LINK"),
+		            "CHECK_DATES" => "Y",
+		            "DETAIL_URL" => "",
+		            "AJAX_MODE" => "N",
+		            "AJAX_OPTION_JUMP" => "N",
+		            "AJAX_OPTION_STYLE" => "Y",
+		            "AJAX_OPTION_HISTORY" => "N",
+		            "CACHE_TYPE" => "A",
+		            "CACHE_TIME" => "3600",
+		            "CACHE_FILTER" => "N",
+		            "CACHE_GROUPS" => "Y",
+		            "PREVIEW_TRUNCATE_LEN" => "",
+		            "ACTIVE_DATE_FORMAT" => "d.M.Y",
+		            "SET_STATUS_404" => "Y",
+		            "SET_TITLE" => "N",
+		            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		            "ADD_SECTIONS_CHAIN" => "Y",
+		            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		            "PARENT_SECTION" => "",
+		            "PARENT_SECTION_CODE" => "",
+		            "INCLUDE_SUBSECTIONS" => "Y",
+		            "DISPLAY_DATE" => "Y",
+		            "DISPLAY_NAME" => "Y",
+		            "DISPLAY_PICTURE" => "Y",
+		            "DISPLAY_PREVIEW_TEXT" => "Y",
+		            "PAGER_TEMPLATE" => "page",
+		            "DISPLAY_TOP_PAGER" => "N",
+		            "DISPLAY_BOTTOM_PAGER" => "Y",
+		            "PAGER_TITLE" => "Новости",
+		            "PAGER_SHOW_ALWAYS" => "N",
+		            "PAGER_DESC_NUMBERING" => "N",
+		            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		            "PAGER_SHOW_ALL" => "N"
+		        )
+		    );?>
+		</div>
+	</div>
+</div>
+<?
+} elseif ($section_code == "brands" && $subsection_code != false) {
+    //Для начала надо проверить, а есть ли такой бренд
+    //получаем все значения брендов
+    $brandPage = false;
+    $BRANDS_QUERY = CIBlockPropertyEnum::GetList(
+        Array(
+            "SORT" => "ASC",
+            "VALUE" => "ASC"
+        ),
+        Array(
+            "IBLOCK_ID" => "17",
+            "CODE" => "BRAND"
+        )
+    );
+    $i = 0;
+    while (($BRAND = $BRANDS_QUERY->Fetch()) != false) {
+        $BRANDS[$i] = $BRAND;
 
-if ($section_code != "new" && $section_code != "sale" && !$brandFilter) {
+//    test_dump(strtolower($BRAND["XML_ID"]) . " != " . strtolower($third_section_code));
+
+
+        if (strtolower($BRAND["XML_ID"]) == strtolower($subsection_code)) {
+            $brandFilter = true;
+            $CURRENT_BRAND1 = $BRAND;
+        }
+
+        $i++;
+    }
+//=================================
+    if ($CURRENT_BRAND1 == false) {
+        include($_SERVER["DOCUMENT_ROOT"] . "/404.php");
+        die();
+    }
+
+    $GLOBALS["arrFilterAjaxSection"] = array(
+        "PROPERTY_BRAND_VALUE" => array(
+            "0" => strtoupper($subsection_code)
+        )
+    );
+
+    $APPLICATION->IncludeComponent(
+        "bitrix:catalog.section",
+        "section_items_brand",
+        Array(
+            "IBLOCK_TYPE" => "catalog",
+            "IBLOCK_ID" => "17",
+            "SECTION_ID" => "",
+            "SECTION_CODE" => "",
+            "SECTION_USER_FIELDS" => array(""),
+            "ELEMENT_SORT_FIELD" => "sort",
+            "ELEMENT_SORT_ORDER" => "asc",
+            "ELEMENT_SORT_FIELD2" => "id",
+            "ELEMENT_SORT_ORDER2" => "desc",
+            "FILTER_NAME" => "arrFilterAjaxSection",
+            "INCLUDE_SUBSECTIONS" => "Y",
+            "SHOW_ALL_WO_SECTION" => "Y",
+            "HIDE_NOT_AVAILABLE" => "N",
+            "PAGE_ELEMENT_COUNT" => "24",
+            "LINE_ELEMENT_COUNT" => "1",
+            "PROPERTY_CODE" => array("PRICE", "NEW"),
+            "OFFERS_LIMIT" => "5",
+            "TEMPLATE_THEME" => "",
+            "PRODUCT_SUBSCRIPTION" => "N",
+            "SHOW_DISCOUNT_PERCENT" => "N",
+            "SHOW_OLD_PRICE" => "N",
+            "SHOW_CLOSE_POPUP" => "N",
+            "MESS_BTN_BUY" => "Купить",
+            "MESS_BTN_ADD_TO_BASKET" => "В корзину",
+            "MESS_BTN_SUBSCRIBE" => "Подписаться",
+            "MESS_BTN_DETAIL" => "Подробнее",
+            "MESS_NOT_AVAILABLE" => "Нет в наличии",
+            "SECTION_URL" => "",
+            "DETAIL_URL" => "",
+            "SECTION_ID_VARIABLE" => "SECTION_ID",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "N",
+            "AJAX_OPTION_HISTORY" => "N",
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "36000000",
+            "CACHE_GROUPS" => "Y",
+            "SET_TITLE" => "Y",
+            "SET_BROWSER_TITLE" => "Y",
+            "BROWSER_TITLE" => "-",
+            "SET_META_KEYWORDS" => "Y",
+            "META_KEYWORDS" => "-",
+            "SET_META_DESCRIPTION" => "Y",
+            "META_DESCRIPTION" => "-",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "SET_STATUS_404" => "N",
+            "CACHE_FILTER" => "N",
+            "ACTION_VARIABLE" => "action",
+            "PRODUCT_ID_VARIABLE" => "id",
+            "PRICE_CODE" => array("BASE"),
+            "USE_PRICE_COUNT" => "N",
+            "SHOW_PRICE_COUNT" => "1",
+            "PRICE_VAT_INCLUDE" => "Y",
+            "CONVERT_CURRENCY" => "N",
+            "BASKET_URL" => "/personal/cart/",
+            "USE_PRODUCT_QUANTITY" => "N",
+            "ADD_PROPERTIES_TO_BASKET" => "Y",
+            "PRODUCT_PROPS_VARIABLE" => "prop",
+            "PARTIAL_PRODUCT_PROPERTIES" => "N",
+            "PRODUCT_PROPERTIES" => array(),
+            "ADD_TO_BASKET_ACTION" => "ADD",
+            "DISPLAY_COMPARE" => "N",
+            "PAGER_TEMPLATE" => "page",
+            "DISPLAY_TOP_PAGER" => "N",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "PAGER_TITLE" => "Товары",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N"
+        )
+    );
+
+} elseif ($section_code != "new" && $section_code != "sale" && !$brandFilter) {
     if ($subsection_code == false)
         $template = "section_items";
     if ($section_code != false && $subsection_code != false)
@@ -179,7 +362,7 @@ if ($section_code != "new" && $section_code != "sale" && !$brandFilter) {
         )
     );
 
-    if ($template == "section_items") {
+    if ($template == "section_catalog") {
         ?>
         </div>
         </div>
