c69c83314fef25861125498ae99b3bdb5b73095f
m-rustam1996 <m-rustam1996@rambler.ru>
2015-12-23 00:27:40 +0400

    Task 7.1

--- bitrix/.settings.php
+++ bitrix/.settings.php
@@ -5,15 +5,15 @@ return array (
             'value' =>
                 array (
                     'debug' => true,
-                    'handled_errors_types' =>   E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED,
-                    'exception_errors_types' =>   E_ALL & E_NOTICE & ~E_STRICT & ~E_DEPRECATED,
+                    'handled_errors_types' => 4437,
+                    'exception_errors_types' => 4437,
                     'ignore_silence' => false,
                     'assertion_throws_exception' => true,
                     'assertion_error_type' => 256,
                     'log' => array (
                         'settings' =>
                             array (
-                                'file' => '/home/bitrix/www/log/exceptions.log',
+                                'file' => '/var/log/php/exceptions.log',
                                 'log_size' => 1000000,
                             ),
                     ),
